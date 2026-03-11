82097b375ac153d4740347e5778c62b6cea934dd
Smorodin_SA <ssmorodin@bk.ru>
2026-03-12 01:41:58 +0300

    стили для списка с файлами

--- my-app/src/components/FilesStorage/FileList.jsx
+++ my-app/src/components/FilesStorage/FileList.jsx
@@ -12,34 +12,40 @@ const FileList = ({ files, onFileAction, formatDate, formatFileSize }) => {
   }
 
   return (
-    <table className="table">
+    <table className="table file-list-table">
       <thead>
         <tr>
-          <th>Имя файла</th>
-          <th>Комментарий</th>
-          <th>Размер</th>
-          <th>Дата загрузки</th>
-          <th>Последнее скачивание</th>
-          <th>Статус</th>
-          <th>Действия</th>
+          <th className="col-name">Имя файла</th>
+          <th className="col-comment">Комментарий</th>
+          <th className="col-size">Размер</th>
+          <th className="col-date">Дата загрузки</th>
+          <th className="col-download">Последнее скачивание</th>
+          <th className="col-status">Статус</th>
+          <th className="col-actions">Действия</th>
         </tr>
       </thead>
       <tbody>
         {files.map(file => (
           <tr key={file.id}>
-            <td>{file.original_name}</td>
-            <td>{file.comment || '-'}</td>
-            <td>{formatFileSize(file.size)}</td>
-            <td>{formatDate(file.upload_date)}</td>
-            <td>{formatDate(file.last_download)}</td>
-            <td>
+            <td className="col-name file-list-cell-ellipsis" title={file.original_name}>
+              {file.original_name}
+            </td>
+            
+            <td className="col-comment file-list-cell-clamp" title={file.comment}>
+              {file.comment || '-'}
+            </td>
+            
+            <td className="col-size">{formatFileSize(file.size)}</td>
+            <td className="col-date">{formatDate(file.upload_date)}</td>
+            <td className="col-download">{formatDate(file.last_download)}</td>
+            <td className="col-status">
               {file.share_token ? (
                 <span className="file-list-status-shared">🔗 share</span>
               ) : (
                 <span className="file-list-status-not-shared">not share</span>
               )}
             </td>
-            <td>
+            <td className="col-actions">
               <FileActions 
                 file={file}
                 onFileAction={onFileAction}
